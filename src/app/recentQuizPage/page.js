59d6ee45b12cfc03aa44523135ee998a58643d86
Mohammed Zaheer <152991044+MohammedZaheer0@users.noreply.github.com>
2024-06-25 20:46:47 +0530

    commited

--- src/app/recentQuizPage/page.js
+++ src/app/recentQuizPage/page.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Image from 'next/image';
-import RecentQuizImg from '../../Assets/images/recentQuizImg.png'
+import RecentQuizImg from '../../Assets/images/recentQuizImg.png';
+import ThumbsUp from '../../Assets/images/thumbs-up 1.png'
+import ThumbsDown from '../../Assets/images/thumbs-down 1.png'
 
 const RecentQuizPage = () => {
   return (
     <>
-        <div>
+        <div className='px-3'>
                 <h2 className='text-[#1935CA] font-bold'>Recent Quizes</h2>
-                <div className='my-4 grid grid-cols-2'>
-                <div className='w-[85%]'>
+                <div className=' my-4 grid lg:grid-cols-[60%_40%] grid-cols-1'>
+                <div className='lg:w-[80%] w-[95%] '>
                   <div className='cursor-pointer'>
                   <Image 
                     src={RecentQuizImg}
@@ -18,7 +20,34 @@ const RecentQuizPage = () => {
                     height='full'
                   />
                   </div>
-                  <p className='my-3 font-medium'>Protecting the Organisation from Cyber Attacks</p>
+
+                  <div className='flex justify-between items-center w-[100%]'>
+                  <p className='my-3 font-medium lg:w-[70%] xl:w-[80%] w-[70%] sm:w-[80%]'>Protecting the Organisation from Cyber Attacks</p>
+
+                  <div className='flex lg:w-[30%] xl:w-[20%] w-[30%] sm:w-[20%] items-center justify-between'>
+                    <div className='flex  items-center text-[#6FD181]'>
+                      <div className='mr-1'><Image 
+                        src={ThumbsUp}
+                        priority
+                        alt='ThumbsUp'
+                        width='full'
+                        height='full'
+                      /></div>
+                      <small>145</small>
+                    </div>
+                    
+                    <div className='ml-4 flex items-center text-[#DFDFE3]'>
+                      <div className='mr-1'><Image 
+                        src={ThumbsDown}
+                        priority
+                        alt='ThumbsUp'
+                        width='full'
+                        height='full'
+                      /></div>
+                      <small>6</small>
+                    </div>
+                  </div>
+                  </div>
 
                   <p className='my-3 text-[13px] tracking-wide'>One of the most efficient ways to protect against cyber attacks and all types of data breaches is to train your employees on cyber attack prevention and</p>
 
@@ -26,7 +55,7 @@ const RecentQuizPage = () => {
                   </div>
 
              <div>
-             <div className='my-3 text-[#696F79]'>
+             <div className='my-3 text-[#696F79] text-[14px] md:text-[16px] '>
                     <div className='flex justify-between'>
                       <h2 className='font-bold'>Date : </h2>
                       <p>28/07/2021</p>
@@ -34,7 +63,7 @@ const RecentQuizPage = () => {
 
                     <div className='flex justify-between my-4'>
                       <h2 className='font-bold'>Time Limit: </h2>
-                      <p>Time Limit: </p>
+                      <p>15 Mins </p>
                     </div>
 
                     <div className='flex justify-between my-4'>
